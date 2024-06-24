2a92e5178c121865b025e9ca1a6881fb292959cf
우종현 <whdgus616@gmail.com>
2024-06-24 14:49:20 +0900

    winFrom 프로젝트 업로드

---
+++ ConvertXlsxToXML/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ConvertXlsxToXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(345, 148);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(133, 45);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload Excel File";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(345, 232);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(133, 50);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert to XML";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(329, 314);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 12);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ ConvertXlsxToXML/Form1.cs
@@ -0,0 +1,76 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ConvertXlsxToXML
+{
+    public partial class Form1 : Form
+    {
+        private string excelFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Excel Files|*.xls;*.xlsx";
+                openFileDialog.Title = "Select an Excel File";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    excelFilePath = openFileDialog.FileName;
+                    lblStatus.Text = "File Uploaded: " + openFileDialog.SafeFileName;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(excelFilePath))
+            {
+                MessageBox.Show("Please upload an Excel file first.");
+                return;
+            }
+
+            try
+            {
+                var workbook = new XLWorkbook(excelFilePath);
+                var worksheet = workbook.Worksheets.First();
+
+                var xml = new XElement("root",
+                    from row in worksheet.RowsUsed().Skip(1)
+                    let cells = row.CellsUsed().Select(c => new XElement(worksheet.Cell(1, c.Address.ColumnNumber).GetValue<string>(), c.GetValue<string>()))
+                    select new XElement("row", cells)
+                );
+
+                string xmlFilePath = System.IO.Path.ChangeExtension(excelFilePath, ".xml");
+                xml.Save(xmlFilePath);
+                lblStatus.Text = "Converted to XML: " + xmlFilePath;
+                MessageBox.Show("Excel file has been converted to XML.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
